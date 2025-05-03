0d43feae6b42215efb62cbea21390ea0b23c8687
Shvalev Anton <shvalev_as88@mail.ru>
2025-05-04 00:46:03 +0300

    Signed-off-by: Shvalev Anton <shvalev_as88@mail.ru>

---
+++ Lab3/Ex1/WebMVCR/WebMVCR/Controllers/MyController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVCR.Models;
+
+namespace WebMVCR.Controllers
+{
+    public class MyController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index(string hel)
+        {
+            int hour = DateTime.Now.Hour;
+            string Greeting = ModelClass.ModelHello() + ", " + hel;
+            return Greeting;
+        }
+    }
+}
