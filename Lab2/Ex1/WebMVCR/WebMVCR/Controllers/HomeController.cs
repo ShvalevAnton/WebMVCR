809292662ae748816f3dfd02bf59e53f8f0a4b70
Shvalev Anton <shvalev_as88@mail.ru>
2025-05-03 20:43:36 +0300

    Signed-off-by: Shvalev Anton <shvalev_as88@mail.ru>

---
+++ Lab2/Ex1/WebMVCR/WebMVCR/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVCR.Models;
+
+namespace WebMVCR.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index()
+        {
+            //string Greeting = ModelClass.ModelHello();
+            //string res = ExeEnum();
+            string res = ExeStruct();
+            return res;
+        }
+
+        public string ExeEnum()
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+
+            string res1 = String.Format("Тип банковского счета {0}", goldAccount);
+            string res2 = String.Format("Тип банковского счета {0}", platinumAccount);
+            string res = res1 + "<p>" + res2;
+            return res;
+        }
+
+        public string ExeStruct()
+        {
+            BankAccount goldBankAccount;
+
+            goldBankAccount.accType = AccountType.Checking; 
+            goldBankAccount.accBal = (decimal)3200.00; 
+            goldBankAccount.accNo = 123;
+
+            //string res = String.Format("Номер счета {0}, баланс {1}, тип {2}", goldBankAccount.accNo, goldBankAccount.accBal, goldBankAccount.accType);
+            string res = String.Format("Информация о банковском счете: {0}", goldBankAccount);
+            return res;
+
+        }
+    }
+}
---
+++ Lab2/Ex1/WebMVCR/WebMVCR/Models/StudyCsharp,.cs
@@ -0,0 +1,22 @@
+﻿namespace WebMVCR.Models
+{
+    public enum AccountType { 
+        Checking,   // Текуий расчетный счёт
+        Deposit     // Вклад
+    }
+    public struct BankAccount { 
+        public long accNo; 
+        public decimal accBal; 
+        public AccountType accType;
+
+        public override string ToString() 
+        { 
+            string res = String.Format("Номер счета {0}, баланс {1}, тип {2}", accNo, accBal, accType); 
+            return res; 
+        }
+    }
+    public class StudyCsharp_
+    {
+
+    }
+}
