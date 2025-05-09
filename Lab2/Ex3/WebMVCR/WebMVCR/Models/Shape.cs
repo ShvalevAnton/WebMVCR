74991ef514b88b8a6c1634883fc6e0cca46de790
Shvalev Anton <shvalev_as88@mail.ru>
2025-05-09 17:16:10 +0300

    Signed-off-by: Shvalev Anton <shvalev_as88@mail.ru>

---
+++ Lab2/Ex3/WebMVCR/WebMVCR/Models/Shape.cs
@@ -0,0 +1,14 @@
+﻿namespace WebMVCR.Models
+{
+    public class Shape
+    {
+        public double St { get; set; }
+        virtual public string Name 
+        { 
+            get 
+            { 
+                return String.Format("\"Фигура\""); 
+            } 
+        }
+    }
+}
---
+++ Lab2/Ex3/WebMVCR/WebMVCR/Models/Triangle.cs
@@ -0,0 +1,47 @@
+﻿namespace WebMVCR.Models
+{
+    public class Triangle : Shape
+    {
+        //public double Sta { get; set; }
+        public double Stb { get; set; }
+        public double Stc { get; set; }
+
+        //public string Name
+        //{
+        //    get
+        //    {
+        //        return String.Format("\"Треугольник со сторонами {0}, {1} и {2}\"", St, Stb, Stc);
+        //    }
+        //}
+        override public string Name 
+        { 
+            get 
+            { 
+                return String.Format("\"Треугольник со сторонами {0}, {1} и {2}\"", St, Stb, Stc); 
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                double p = St + Stb + Stc; 
+                return p;
+            }
+        }
+        public double Area
+        {
+            get
+            {
+                double sq = Math.Sqrt(Perimeter / 2 * (Perimeter / 2 - St) * (Perimeter / 2 - Stb) * (Perimeter / 2 - Stc));
+                return sq;
+            }
+        }
+
+        public Triangle(double a, double b, double c) 
+        { 
+            St = a; 
+            Stb = b; 
+            Stc = c; 
+        }
+    }
+}
