c51b260b79d9f9aff7388c2db9944ae65e23faa2
Shvalev Anton <shvalev_as88@mail.ru>
2025-05-09 16:09:08 +0300

    Signed-off-by: Shvalev Anton <shvalev_as88@mail.ru>

---
+++ Lab6/Ex2/MvcCreditApp/MvcCreditApp/Models/Bid.cs
@@ -0,0 +1,15 @@
+﻿namespace MvcCreditApp.Models
+{
+    // Класс реализует модель данных о заявке на кредит.
+    public class Bid
+    {
+        // ID заявки 
+        public virtual int BidId { get; set; } 
+        // Имя заявителя
+        public virtual string Name { get; set; } 
+        // Название кредита 
+        public virtual string CreditHead { get; set; } 
+        // Дата подачи заявки 
+        public virtual DateTime bidDate { get; set; }
+    }
+}
